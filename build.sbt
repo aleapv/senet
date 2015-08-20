9f13aa06504e2853bb084906bac450722e641f89
unknown <aleapv@mail.ru>
2015-08-20 21:54:55 +0600

    Setup UI in Scala.js

--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.4"
+libraryDependencies ++= Seq(
+  "org.scalacheck" %% "scalacheck" % "1.12.4",
+  "org.scala-js" %%% "scalajs-dom" % "0.8.0",
+  "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+)
 
 enablePlugins(ScalaJSPlugin)
 
 scalaJSStage in Global := FastOptStage
+
+skip in packageJSDependencies := false
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -4,9 +4,17 @@ package tutorial.webapp
  * Created by aleapv on 20.08.2015.
  */
 import scala.scalajs.js.JSApp
+import org.scalajs.jquery.jQuery
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
+    jQuery(setupUI _)
+  }
+  def addClickedMessage(): Unit = {
+    jQuery("body").append("<p>You clicked the button!</p>")
+  }
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Hello World</p>")
   }
 }
