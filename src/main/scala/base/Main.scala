0dd106d5ce50db94d78c3245c7624ac552c320d1
unknown <aleapv@mail.ru>
2015-08-30 00:14:55 +0600

    add render desk

--- src/main/scala/base/Main.scala
+++ src/main/scala/base/Main.scala
@@ -24,6 +24,16 @@ object Main {
     var whoseMove = 1 // 1 - first, 2- second
     var firstMove = 0
     var count = 0
+    var desk = new Array[(Int,String)](30)
+    var i = 0;
+    for(i <- 1 to 30) {
+      desk(i-1) = (i,i.toString)
+    }
+    desk(14) = (15,"φ") 
+    desk(25) = (26,"V") 
+    desk(26) = (27,"≡")
+    desk(27) = (28,"%")
+    desk(28) = (29,"Θ")
 
     def run() = {
       renderer.clearRect(0, 0, canvas.width, canvas.height)
@@ -34,7 +44,7 @@ object Main {
 
       renderer.fillText(" count - " + count, 100, 30)
 
-      if(firstMove != 0) renderer.fillText("firstMove - " + firstMove, 100, 80)
+      if(firstMove != 0) renderer.fillText("firstMove φ- " + firstMove, 100, 80)
 
       List(1,2,3,4).foldLeft(count)((st: Int, x: Int) => {
           if(st > 0 && st < 5) {
@@ -46,6 +56,23 @@ object Main {
           st - 1
         }
       )
+
+      // render desk
+      for(j <- 0 to 2) {
+        for(i <- 1 to 10) {
+          val s = 50
+          val x = 90 + i*s
+          val y = 130 + j*s
+          renderer.fillStyle = "yellow"
+          renderer.fillRect(x, y, s - 2, s - 2)
+          renderer.fillStyle = "blue"
+          if(j == 1) {
+            renderer.fillText(desk(30 - i - j*10)._2, x + 15, y + 15)
+          } else {
+            renderer.fillText(desk(i - 1 + j*10)._2, x + 15, y + 15)          
+          }
+        }
+      }
     }
 
     dom.setInterval(run _, 20)
